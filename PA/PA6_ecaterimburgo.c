aa87583243fa772b95bda3327f1edfa997c3e71a
artursbm <artursbmello@gmail.com>
2017-09-22 17:54:50 -0300

    Início da PA 6 e criação de um default.c

---
+++ PA/PA6_ecaterimburgo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ------- PA 5: PRODUÇÃO EM ECATERIMBURGO --------- //
+//               Problema 2115 do URI                //
+// --------------------------------------------------// 
+
+int main() {
+
+
+  
+  return 0;
+}
